a665e43f41caff1d2b8df311e3360cffe9d0ed13
Nick Novoa <nicknovoa03@gmail.com>
2023-07-06 22:13:58 -0500

    add thirdweb provider to app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,28 +1,29 @@
-import React from 'react'
-import Head from 'next/head'
-import 'aos/dist/aos.css'
-import '../theme/globals.css'
-import Page from '../components/Page'
-import { Web3Modal } from '@web3modal/react'
-import { AppProps } from 'next/app'
-import { configureChains, createClient, WagmiConfig } from 'wagmi'
-import { sepolia } from 'wagmi/chains'
-import { EthereumClient, modalConnectors, walletConnectProvider } from '@web3modal/ethereum'
+import React from 'react';
+import Head from 'next/head';
+import 'aos/dist/aos.css';
+import '../theme/globals.css';
+import Page from '../components/Page';
+import { Web3Modal } from '@web3modal/react';
+import { AppProps } from 'next/app';
+import { configureChains, createClient, WagmiConfig } from 'wagmi';
+import { sepolia } from 'wagmi/chains';
+import { EthereumClient, modalConnectors, walletConnectProvider } from '@web3modal/ethereum';
+import { ThirdwebProvider } from '@thirdweb-dev/react';
 
 // 1. Get projectID at https://cloud.walletconnect.com
-const projectId = '72e247ee043a24a6e5ecfa195061a9fd'
+const projectId = '72e247ee043a24a6e5ecfa195061a9fd';
 
 // 2. Configure wagmi client
-const chains = [sepolia]
-const { provider } = configureChains(chains, [walletConnectProvider({ projectId })])
+const chains = [sepolia];
+const { provider } = configureChains(chains, [walletConnectProvider({ projectId })]);
 const wagmiClient = createClient({
   autoConnect: true,
   connectors: modalConnectors({ appName: 'web3Modal', chains }),
   provider
-})
+});
 
 // 3. Configure modal ethereum client
-export const ethereumClient = new EthereumClient(wagmiClient, chains)
+export const ethereumClient = new EthereumClient(wagmiClient, chains);
 
 // 4. Wrap your app with WagmiProvider and add <Web3Modal /> compoennt
 export default function App({ Component, pageProps }: AppProps) {
@@ -34,11 +35,13 @@ export default function App({ Component, pageProps }: AppProps) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <WagmiConfig client={wagmiClient}>
-        <Page>
-          <Component {...pageProps} />
-        </Page>
+        <ThirdwebProvider>
+          <Page>
+            <Component {...pageProps} />
+          </Page>
+        </ThirdwebProvider>
       </WagmiConfig>
       <Web3Modal projectId={projectId} ethereumClient={ethereumClient} />
     </React.Fragment>
-  )
+  );
 }
