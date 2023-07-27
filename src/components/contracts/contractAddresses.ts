5cc2e2bad71f7ba44d3fee98f9706f65ee03d121
Nick Novoa <nicknovoa03@gmail.com>
2023-07-27 17:59:25 -0500

    link pool DI

--- src/components/contracts/contractAddresses.ts
+++ src/components/contracts/contractAddresses.ts
@@ -6,4 +6,4 @@ export const Pool3ContractAddress: `0x${string}` = '0xe9c75492F38Eb4F7391a4A352a
 
 export const PoolPrestigeContractAddress: `0x${string}` = '0x3E7b3b5a34bf4e4c3A4dA81f882172899620e54d';
 
-export const PoolDiContractAddress: `0x${string}` = '0xc35341C0B62df2D3A577476289AA436Bf9927259';
+export const PoolDiContractAddress: `0x${string}` = '0xfF370b631eB703034364C09C94435b9c11128AE4';
